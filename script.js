8007dd3aa603795b453a0a8b62810791317f9412
Alana <alanabeatriztobias@gmail.com>
2026-04-29 13:23:00 -0300

    Classe Tarefa finalizada

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 //1. Criando a classe Tarefa:
+// Serve como um modelo para criar tarefas
+// Exemplo:
+// const tarefa1 = new Tarefa("Estudar javascript")
 
 class Tarefa {
-    constructor(texto)
+    constructor(texto){
+    // O construtor é executado automaticamente
+    // Quando usamos "newTarefa"
 
+this.texto = texto;
 
-
+this.concluida = false
 
 }
+
+// Método toggle()
+// É uma palavra em inglês muito usada na programação
+// Significa - Alternar/trocar/inverter estado
+// Exemplo:
+// Ligado -> desligado
+// Aberto -> fechado
+// True -> false
+
+toggle() {
+
+    // O operador ! significa "inverter"
+    this.concluida = !this.concluida;
+
+    }
+
+}
+
+// Classe App
+// Essa classe controla toda a aplicação
+// Ela é responsável por:
+// - Adicionar tarefas 
+// - Remover tarefas
+// - Marcar tarefas
+// - Atualizar a tela
+
+
+
+
+
